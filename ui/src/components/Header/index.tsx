a8c5ee3056bfc693e4a6f6950eb4f4236ef36347
Greyson Elkins <greysonelkins@gmail.com>
2025-04-11 18:20:29 -0600

    Use Logo component

--- ui/src/components/Header/index.tsx
+++ ui/src/components/Header/index.tsx
@@ -1,28 +1,13 @@
-import { Link, NavLink } from 'react-router'
+import { Link } from 'react-router'
 
-import './Header.scss'
+import Logo from 'components/Logo'
 
-const Header = () => {
-    const links = [
-        { to: '/calendar', children: <i>calendar_today</i> },
-        { to: '/map', children: <i>map</i> },
-        { to: '/user', children: <i>person</i> },
-    ]
-
-    const renderLinks = () => links.map(l => (
-        <NavLink {...l} className={({ isActive }) => isActive ? "active" : ""}/>
-    ))
-
-    return (
-        <header>
-            <Link to="/">
-                <h1>seehoo</h1>
-            </Link>
-            <nav>
-                {renderLinks()}
-            </nav>
-        </header>
-    )
-}
+const Header = () => (
+  <header>
+    <Link to="/">
+      <Logo />
+    </Link>
+  </header>
+)
 
 export default Header 
